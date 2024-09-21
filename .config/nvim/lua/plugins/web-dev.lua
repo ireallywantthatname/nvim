a577b3770bdb2d4ef01234efc926fc71e64816bd
ireallywantthatname <rladsilva@students.nsbm.ac.lk>
2024-09-21 10:12:28 +0530

    Foundation

--- .config/nvim/lua/plugins/web-dev.lua
+++ .config/nvim/lua/plugins/web-dev.lua
@@ -3,8 +3,10 @@ return {
   "mattn/emmet-vim",
   "othree/html5.vim",
   "hail2u/vim-css3-syntax",
-  "vim-javascript",
+  "pangloss/vim-javascript",
   "MaxMEllon/vim-jsx-pretty",
   "windwp/nvim-ts-autotag",
-  "prettier/vim-prettier"
+  "prettier/vim-prettier",
+  "windwp/nvim-ts-autotag",
+  "wolandark/vim-live-server"
 }
